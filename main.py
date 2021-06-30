80950a92d90581867ea2221c836ae6f1b0103c94
cubanezu9999 <71436676+cubanezu9999@users.noreply.github.com>
2021-06-30 13:44:46 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,31 @@
+print ('Flour')
+
+ctr = 0
+
+while(ctr<=3):
+  print('1 Egg') 
+  ctr += 1
+  
+
+print('1 Milk')
+
+ctr = 0
+while(ctr<=2):
+ print('1 Sugar')
+ ctr += 1
+
+print('Baking Soda')
+
+ctr = 0
+
+while(ctr <= 59):
+  print('1 minute bake')
+  ctr += 1 
+
+ctr = 0
+
+while(ctr <= 4):
+ print('Eat Slice')
+ ctr += 1
+
+
